86d58344b4661883c351d389f714691b4b8f7a97
Martin Pereyra <pereyra.gamboa@outlook.com>
2018-11-14 20:23:12 -0600

    Corrección de error mamón de encabezados CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Accept,content-type,application/json');
+  next();
 });
 
 app.use(bodyParser.json());
